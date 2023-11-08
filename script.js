039f4f0f201a04c5f13271221daebe877d5e50c9
AZBLACKPHOENIX <unlimitednigami@gmail.com>
2023-11-08 07:52:24 +0200

    aid_finance_calculator

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
-function calculateROI() {
-    var investmentAmount = parseFloat(document.getElementById("investmentAmount").value);
-    var investmentPeriod = parseInt(document.getElementById("investmentPeriod").value);
-    var returnOnInvestment = investmentAmount * 0.05 * investmentPeriod;
-    document.getElementById("result").textContent = "Return on Investment: R" + returnOnInvestment.toFixed(2);
-}
+document.addEventListener("DOMContentLoaded", function() {
+    const investmentAmountInput = document.getElementById("investmentAmount");
+    const investmentPeriodInput = document.getElementById("investmentPeriod");
+    const periodValue = document.getElementById("periodValue");
+    const roiElement = document.getElementById("roi");
+    const totalAmountElement = document.getElementById("totalAmount");
+
+    investmentAmountInput.addEventListener("input", calculateTotalAmount);
+    investmentPeriodInput.addEventListener("input", updatePeriodValue);
+
+    function calculateTotalAmount() {
+        const investmentAmount = parseFloat(investmentAmountInput.value) || 0;
+        const investmentPeriod = parseInt(investmentPeriodInput.value) || 0;
+
+        if (investmentAmount >= 1 && investmentPeriod >= 1) {
+            const totalAmount = investmentAmount * 0.05 * investmentPeriod;
+            roiElement.textContent = "Return on Investment: R" + totalAmount.toFixed(2);
+            totalAmountElement.textContent = "Total Amount Returned: R" + totalAmount.toFixed(2);
+        } else {
+            roiElement.textContent = "Return on Investment: R0.00";
+            totalAmountElement.textContent = "Total Amount Returned: R0.00";
+        }
+    }
+
+    function updatePeriodValue() {
+        const period = parseInt(investmentPeriodInput.value);
+        periodValue.textContent = period + (period === 1 ? " month" : " months");
+        calculateTotalAmount();
+    }
+});
